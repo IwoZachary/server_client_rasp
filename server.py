f0c91edd64baaefc5a830a15e97c06b46940d7e5
IwoZachary <iwozachary@gmail.com>
2020-03-31 14:37:20 +0200

    Added interface

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import datetime
 import os
+from threading import Thread
+import paho.mqtt.client as mqtt
 class Worker:
     def __init__(self,cardNumb,name):
         self.cardNumb=cardNumb
@@ -11,12 +13,12 @@ class Worker:
         print("Done")
 
 class Unidentified:
-    def __init__(self,):
+    def __init__(self):
         self.file=open("/home/iwo/Desktop/server_client/server_client_rasp/Unidentified/"+"Unauthorisied"+".txt","a")
-        self.file.wirte("List of unauthorisied attempts to connect to the server valid from "+format(datetime.datetime.now())+'\n')
+        self.file.write("List of unauthorisied attempts to connect to the server valid from "+format(datetime.datetime.now())+'\n')
         self.file.close()
         self.setOfUnident={}
-        print("Done1")
+        print("Done")
         
     def addUndefined(self,cardNumb):
         self.setOfUnitendent.add(cardNumb)
@@ -28,32 +30,64 @@ class Unidentified:
             print("Sorry an error occured, which can makes your list leaking data")
 
 class Server:
+    
+
+
     def __init__(self):
         self.workerList=[]
-       # p=Unidentified()
-       # self.unidentified=p
+        self.undidentified=Unidentified()
+        
+
+    
         
     def addWorker(self,cardNum,name):
         self.workerList.append(Worker(cardNum,name))
-        #if cardNum in self.unidentified.setOfUnitendent:
-           # self.unidentified.setOfUnitendent.remove(cardNum)
+        if cardNum in self.unidentified.setOfUnitendent:
+            self.unidentified.setOfUnitendent.remove(cardNum)
     
     def removeWorker(self,num):
         for el in self.workerList:
             if el.cardNumb ==num:
-               # try:
+                try:
                     
                     os.remove("/home/iwo/Desktop/server_client/server_client_rasp/workerFiles/"+format(el.cardNumb)+".txt")
-                #except:
-                    #print("Error occured during expeling "+el.name+" from work!")
                     self.workerList.remove(el)
-        
-   
+                    print("Done")
+                except:
+                    print("Error occured during expeling "+el.name+" from work!")
+                    
+    def reading_func(self):
+        print("Welcome!!! Press 'help' if you don't know what to do!")
+        while True:
+            command = input()
+            if command=="add":
+                name=input("New Worker name: ")
+                cardNum=input("Workers assigned card: ")
+                try:
+                    self.addWorker(int(cardNum),name)
+                except:
+                    print("Try again")
+            
+            if command=="delete":
+                cardNum=input("Woreker assigned card: ")
+                try:
+                    self.removeWorker(int(cardNum))
+                except:
+                    print("Try again")
+            if command=="help":
+                print ("Press 'add' to add new employe or 'delete' to remove him")
+                print ("If you want to exit type 'end'")
+                
+            if command=="end":
+                break
+                    
+                
+            
 s=Server()
-s.addWorker(111,"Adam")
-s.addWorker(112,"Wiki")
-s.addWorker(123,"Iwo")
-s.removeWorker(111)
+s.reading_func()
+
+
+
             
         
                 
